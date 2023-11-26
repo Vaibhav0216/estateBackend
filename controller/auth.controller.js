90f1fe52cbd54eeea06690da78cb1318b6aeb22d
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2023-11-26 15:23:44 +0530

    Add errorHandler function to handle errors in signin page

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
@@ -14,7 +15,7 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully." });
-  } catch (err) {
+  } catch (err) { 
     return next(err);
   }
 };
