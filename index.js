0624ddd4745cd3ea2a5282e942abdbd66107d987
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2023-12-19 23:30:18 +0530

    Update user route added

--- index.js
+++ index.js
@@ -3,29 +3,35 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import signupRouter from "./routes/auth.routes.js";
-dotenv.config({ path: ".env"});
+import userRouter from "./routes/user.routes.js";
+dotenv.config({ path: ".env" });
 
-mongoose.connect(process.env.mongoURL)
+mongoose
+  .connect(process.env.mongoURL)
   .then(() => {
-    console.log("Connected to MongoDB"); 
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
     process.exit(1);
   });
-const app = express(); 
+const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Estate API");
 });
-app.use("/api/auth", signupRouter); 
+
+app.use("/api/auth", signupRouter);
+app.use("/api/user", userRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
@@ -33,7 +39,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT, () => { 
+app.listen(process.env.PORT, () => {
   console.log(`Estate server running on port ${process.env.PORT}`);
 });
- 
