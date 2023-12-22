64963bdd0261b02aacdb470cddcd34325e91212f
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2023-12-22 15:12:57 +0530

    Add deleteListing function and update user.controller.js and listing.routes.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 import bcryptjs from "bcryptjs";
 
 export const updateUser = async (req, res, next) => {
@@ -31,30 +32,28 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) {
-        return next(errorHandler(401, "You can only delete your own account!"));
-    }
-    try {
-        await User.findByIdAndDelete(req.params.id);
-        res.clearCookie("access_token")
-        res.status(200).json("User has been deleted...");
-    } catch (err) {
-        next(err);
-    }
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
+  } catch (error) {
+    next(error);
+  }
 };
 
 
 export const getUserListings = async (req, res, next) => {
-  try{
-      if(req.user.id === req.params.id){
-          const listing = await Listing.find({useRef: req.params.id})
-          res.status(200).json(listing);
-      }else{
-          return next(errorHandler(401,"You can only view your own listings!"))
-      }
-
-  }catch(error){
-
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
 };
 
