d62c4798cb5f16d467b6d83c5135d89ce405a1ac
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2024-01-03 08:49:38 +0530

    complete get all listing api route on search

--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, getListing, updateListing } from "../controller/listing.controller.js";             
+import { createListing, deleteListing, getListing, getListings, updateListing } from "../controller/listing.controller.js";             
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create",verifyUser,createListing);
 router.delete("/delete/:id",verifyUser,deleteListing);
 router.post("/update/:id",verifyUser, updateListing);  
-router.get("/get/:id", getListing);  
+router.get("/get/:id", getListing); 
+router.get("/get", getListings); 
 export default router;                              
